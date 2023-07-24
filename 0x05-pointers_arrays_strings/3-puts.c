352175a059588754b0476063971c59d9b14ac60f
KOOSNATHI <koosthony@outlook.com>
2023-07-24 02:53:09 -0700

    Put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the standard output.
+ * @str: the string to be printed.
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
