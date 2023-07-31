e33466d3a271da82c00aa890dcbede3100248f8f
KOOSNATHI <koosthony@outlook.com>
2023-07-31 14:04:53 -0700

    string spn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,31 @@
 #include "main.h"
-
 /**
- *_strspn - print the consecutive characters of s1 that are in s2
- *@s: Source string
- *@accept: searching string
- *Return: new string
- */
-
+*_strspn - search the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*@s:segment targeted
+*@accept:reference bytes container
+*
+*Return:returns the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int bytes = 0;
+	int i;
 
-	for (j = 0; *(s + j); j++)
+	while (*s)
 	{
-	for (i = 0; *(s + j);  i++)
+		for (i = 0; accept[i]; i++)
 		{
-	if (*(s + j) == *(accept + i))
-			break;
+			if (accept[i] == *s)
+			{
+				bytes++;
+				break;
+			}
+			else if ((accept[i + 1]) == '\0')
+				return (bytes);
 		}
-	if (*(accept + i) == '\0')
-		break;
+		s++;
 	}
-	return (j);
+	return (bytes);
 }
