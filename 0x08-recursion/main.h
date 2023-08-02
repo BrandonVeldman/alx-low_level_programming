ca9a390c32872a8b9dc16b9d258da0e8f988c732
KOOSNATHI <koosthony@outlook.com>
2023-08-02 02:21:08 -0700

    C - Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
+int _puchar(char c);
+void _puts_recursion(char *s);
+int _strlen_recusrion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H_INCLUDED */
