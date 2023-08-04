55643e7bbb9f16a2cfe0fab4d1e297f1edbf21c5
KOOSNATHI <koosthony@outlook.com>
2023-08-04 13:14:01 -0700

    add 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,18 +1,35 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - multiplies two numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - adds two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int i, j, add = 0;
+
+	if (argc > 1)
 	{
-		printf("Error\n");
-		return (1);
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else if (argc == 1)
+		printf("0\n");
+
 	return (0);
 }
