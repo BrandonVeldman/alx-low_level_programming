5b0b1dab2358b3bb90e20e23808faeebcab3d47e
KOOSNATHI <koosthony@outlook.com>
2023-08-04 13:03:56 -0700

    args 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
+
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-int i, j, add = 0;
 
-for (i = 1; i < argc; i++)
+/*Declaring variables*/
+int count = 0;
+
+if (argc > 0)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
+/*WHILE - Print each arguments*/
+while (count < argc)
 {
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
+printf("%s\n", argv[count]);
+count++;
 }
 }
-add += atoi(argv[i]);
-}
-printf("%d\n", add);
 return (0);
 }
