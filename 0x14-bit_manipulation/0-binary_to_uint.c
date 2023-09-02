1b48b64d7659403b745b61698735340016937d49
KOOSNATHI <veldmankoosanthony@gmail.com>
2023-09-02 12:29:44 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,34 @@
 #include "lists.h"
 
 /**
- * binary_to_uint - This funtion convert binary to decimal
- * @b: This is my string of entry
- * Return: This return to a digital number
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int multiplicador = 1, index = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
-	for (index = 0; b[index] != '\0'; index++)
-	{
-		if (b[index] != '0' && b[index] != '1')
-			return (0);
-	}
-	for (index = index - 1; index >= 0; index--)
-	{
-		char currentCharacter;
 
-		currentCharacter = b[index];
-		if (currentCharacter == '1')
-		{
-			decimal += multiplicador;
-		}
-		multiplicador = multiplicador * 2;
-		if (index == 0)
-			break;
+	while (b[i + 1])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
 	}
-	return (decimal);
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
